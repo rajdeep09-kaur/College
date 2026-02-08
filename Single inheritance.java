0a8e073a0b0e0cd7e5cb5428e993d644abb016d6
Rajdeep kaur <rajdeepsep1@gmail.com>
2026-02-08 18:26:00 +0530

    Create Single inheritance.java

---
+++ Single inheritance.java
@@ -0,0 +1,19 @@
+class Animal {
+    void speak() {
+        System.out.println("The animal makes a sound");
+    }
+}
+
+class Dog extends Animal {
+    void bark() {
+        System.out.println("The dog barks");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.speak();  
+        d.bark();   // Dog's own method
+    }
+}
