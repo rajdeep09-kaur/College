f70d1e6a4689f19d3216c97e0bb6f8291d3b7b81
Rajdeep kaur <rajdeepsep1@gmail.com>
2026-02-12 21:44:30 +0530

    Create Java End-of-file.java

---
+++ Java End-of-file.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n =1;
+        while(sc.hasNextLine()){
+            
+        String line =sc.nextLine();
+        System.out.println(n +" "+ line);
+        n++;
+        }
+        sc.close();
+       
+    }
+}
